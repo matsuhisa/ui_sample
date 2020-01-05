737440f3b5dc8b942c01afafed25192f6817f7a9
matsuhisa <matsuhisa+github@gmail.com>
2020-01-06 00:32:49 +0900

    Tab animation

--- source/javascripts/controllers/aria-selected_controller.js
+++ source/javascripts/controllers/aria-selected_controller.js
@@ -10,26 +10,50 @@ export default class extends Controller {
   toggle() {
     const targetElement = event.currentTarget
     const beforeElement = this.itemTargets.find( item => item.getAttribute("aria-selected") === "true" )
-    const beforeElementUnderline = beforeElement.getElementsByClassName("tablist__tab-underline")[0]
+    const targetElementUnderline = targetElement.getElementsByClassName("tablist__tab-underline")[0]
+    // const beforeElementUnderline = beforeElement.getElementsByClassName("tablist__tab-underline")[0]
     const beforeRect = beforeElement.getBoundingClientRect()
     const currentRect = targetElement.getBoundingClientRect()
-    const x = currentRect["x"] - beforeRect["x"]
+    const x = beforeRect["x"] - currentRect["x"]
+
+    // console.log(currentRect)
+    // console.log(beforeRect)
 
     this.itemTargets.forEach( item => {
-      item.setAttribute("aria-selected", "false")
+      item.getElementsByClassName("tablist__tab-underline")[0].setAttribute("style", "")
     } )
 
-    if(beforeElementUnderline !== undefined) {
-      beforeElementUnderline.setAttribute("style", `transform: translate(${x}px, 0px)`)
-      beforeElementUnderline.addEventListener("transitionend", () => {
-        beforeElementUnderline.setAttribute("style", "")
-        console.log(this)
-      })
-    }else{
-      // targetElement.setAttribute("aria-selected", "true")
-    }
+    targetElementUnderline.setAttribute("style", `transform: translate(${x*-1}px, 0px);position: absolute; width: 100%; left: ${x}px;`)
     targetElement.setAttribute("aria-selected", "true")
+    beforeElement.setAttribute("aria-selected", "false")
+    // targetElementUnderline.setAttribute("style", ``)
   }
+
+  // toggle() {
+  //   const targetElement = event.currentTarget
+  //   const beforeElement = this.itemTargets.find( item => item.getAttribute("aria-selected") === "true" )
+  //   const beforeElementUnderline = beforeElement.getElementsByClassName("tablist__tab-underline")[0]
+  //   const beforeRect = beforeElement.getBoundingClientRect()
+  //   const currentRect = targetElement.getBoundingClientRect()
+  //   const x = currentRect["x"] - beforeRect["x"]
+
+  //   this.itemTargets.forEach( item => {
+  //     item.setAttribute("aria-selected", "false")
+  //     item.getElementsByClassName("tablist__tab-underline")[0].setAttribute("style", "")
+  //   } )
+
+  //   if(beforeElementUnderline !== undefined) {
+  //     beforeElementUnderline.setAttribute("style", `transform: translate(${x}px, 0px)`)
+  //     beforeElementUnderline.addEventListener("transitionend", () => {
+  //       // beforeElementUnderline.setAttribute("style", "")
+  //       targetElement.setAttribute("aria-selected", "true")
+  //       console.log(this)
+  //     })
+  //   }else{
+  //     // targetElement.setAttribute("aria-selected", "true")
+  //   }
+  //   // targetElement.setAttribute("aria-selected", "true")
+  // }
 
   __set(element, value){
     console.log(element)
